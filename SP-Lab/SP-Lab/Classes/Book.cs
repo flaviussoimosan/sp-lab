b557f76909e20806948dc471a9e55bd53b21a64e
flavius-soimosan <flavius.soimosan@yahoo.com>
2022-10-05 08:56:07 +0300

    LAB 1

---
+++ SP-Lab/SP-Lab/Classes/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SP_Lab.Classes
+{
+    public class Book
+    {
+        public List<string> BookContent { get; set; }
+        public Book()
+        {
+            BookContent = new List<string>();
+        }
+
+        public void CreateNewParagraph (string paragraph)
+        {
+            this.BookContent.Add(paragraph);
+        }
+
+        public void CreateNewImage(string image)
+        {
+            this.BookContent.Add(image);
+        }
+
+        public void CreateNewTable(string table)
+        {
+            this.BookContent.Add(table);
+        }
+
+        public void Print()
+        {
+            foreach (string row in BookContent)
+            {
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
---
+++ SP-Lab/SP-Lab/Program.cs
@@ -0,0 +1,23 @@
+﻿using SP_Lab.Classes;
+using System;
+
+namespace SP_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Book book = new Book();
+            book.CreateNewParagraph("paragraph 1");
+            book.CreateNewParagraph("paragraph 2");
+            book.CreateNewParagraph("paragraph 3");
+            book.CreateNewImage("image");
+            book.CreateNewParagraph("paragraph 4");
+            book.CreateNewTable("table");
+            book.CreateNewParagraph("paragraph 5");
+
+            book.Print();
+        }
+    }
+}
