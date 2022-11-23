3413da551cd97faae590552d051fca532110bbee
flavius-soimosan <flavius.soimosan@seltronix.eu>
2022-11-23 08:15:56 +0200

    TEST - Added test project

---
+++ SP-Lab/SP-Lab/Classes/Element.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SP_Lab.Interfaces;
+
+namespace SP_Lab.Classes
+{
+    public class Element <T> : IElement<T>
+    {
+        public void Add(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get(T t)
+        {
+            return;
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SP-Lab/SP-Lab/Interfaces/IElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SP_Lab.Interfaces
+{
+    public interface IElement<T>
+    {
+        public void Add(T t);
+        public void Remove(T t);
+        public void Get(T t);
+
+        public void Print();
+    }
+}
